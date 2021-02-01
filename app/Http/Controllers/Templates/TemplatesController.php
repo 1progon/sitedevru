1b539c19e1d7c91d63c7446099408a24e2ee7587
Alex <gageviolo@gmail.com>
2021-02-01 22:20:07 +0500

    Add TemplatesController

---
+++ app/Http/Controllers/Templates/TemplatesController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers\Templates;
+
+use App\Http\Controllers\Controller;
+use App\Model\Template\Template;
+use Illuminate\Http\Request;
+
+class TemplatesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $templates = Template::paginate();
+
+        return view('templates.index',compact('templates'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  Template  $template
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Template $template)
+    {
+        $template = Template::find($template->id);
+
+        return view('templates.show', compact('template'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
