a354ba0ca4fa94e887a53f92039c1352dd58049d
Alex <gageviolo@gmail.com>
2021-02-01 22:19:46 +0500

    Add Services Controller

---
+++ app/Http/Controllers/Services/ServicesController.php
@@ -0,0 +1,89 @@
+<?php
+
+namespace App\Http\Controllers\Services;
+
+use App\Http\Controllers\Controller;
+use App\Model\Service\Service;
+use Illuminate\Http\Request;
+
+class ServicesController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $services = Service::paginate();
+
+        return view('service.index', compact('services'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Service $service)
+    {
+        $service = Service::find($service->id);
+
+        return view('service.show', compact('service'));
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
