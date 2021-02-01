0ff5cbf180cfaf7c1e1ce93f56adfd176450188c
Alex <gageviolo@gmail.com>
2021-02-01 22:26:17 +0500

    Update sass, css and public files

--- public/assets/js/main.js
+++ public/assets/js/main.js
@@ -1,18 +1,13 @@
-(function ($) {
-    "use strict"
+"use strict"
 
-    /* 1. Proloder */
-    $(window).on('load', function () {
-        $('#preloader-active').delay(450).fadeOut('slow');
-        $('body').delay(450).css({
-            'overflow': 'visible'
-        });
-    });
+testimonials();
+slickMenu();
+scrollToUp();
 
 
-    /* 2. slick Nav */
-// mobile_menu
-    var menu = $('ul#navigation');
+/* Slick Nav - Mobile Menu*/
+function slickMenu() {
+    let menu = $('ul#navigation');
     if (menu.length) {
         menu.slicknav({
             prependTo: ".mobile_menu",
@@ -20,76 +15,11 @@
             openedSymbol: '-'
         });
     }
-    ;
+}
 
 
-    /* 3. MainSlider-1 */
-    function mainSlider() {
-        var BasicSlider = $('.slider-active');
-        BasicSlider.on('init', function (e, slick) {
-            var $firstAnimatingElements = $('.single-slider:first-child').find('[data-animation]');
-            doAnimations($firstAnimatingElements);
-        });
-        BasicSlider.on('beforeChange', function (e, slick, currentSlide, nextSlide) {
-            var $animatingElements = $('.single-slider[data-slick-index="' + nextSlide + '"]').find('[data-animation]');
-            doAnimations($animatingElements);
-        });
-        BasicSlider.slick({
-            autoplay: false,
-            autoplaySpeed: 10000,
-            dots: false,
-            fade: true,
-            arrows: false,
-            prevArrow: '<button type="button" class="slick-prev"><i class="ti-shift-left"></i></button>',
-            nextArrow: '<button type="button" class="slick-next"><i class="ti-shift-right"></i></button>',
-            responsive: [{
-                breakpoint: 1024,
-                settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1,
-                    infinite: true,
-                }
-            },
-                {
-                    breakpoint: 991,
-                    settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1,
-                        arrows: false
-                    }
-                },
-                {
-                    breakpoint: 767,
-                    settings: {
-                        slidesToShow: 1,
-                        slidesToScroll: 1,
-                        arrows: false
-                    }
-                }
-            ]
-        });
-
-        function doAnimations(elements) {
-            var animationEndEvents = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
-            elements.each(function () {
-                var $this = $(this);
-                var $animationDelay = $this.data('delay');
-                var $animationType = 'animated ' + $this.data('animation');
-                $this.css({
-                    'animation-delay': $animationDelay,
-                    '-webkit-animation-delay': $animationDelay
-                });
-                $this.addClass($animationType).one(animationEndEvents, function () {
-                    $this.removeClass($animationType);
-                });
-            });
-        }
-    }
-
-    // mainSlider();
-
-
-    /* 4. Testimonial Active*/
+/* 4. Testimonial Active*/
+function testimonials() {
     var testimonial = $('.h1-testimonial-active');
     if (testimonial.length) {
         testimonial.slick({
@@ -98,7 +28,7 @@
             speed: 1000,
             autoplay: true,
             loop: true,
-            arrows: false,
+            arrows: true,
             prevArrow: '<button type="button" class="slick-prev"><i class="ti-angle-left"></i></button>',
             nextArrow: '<button type="button" class="slick-next"><i class="ti-angle-right"></i></button>',
             slidesToShow: 1,
@@ -133,68 +63,11 @@
             ]
         });
     }
+}
 
 
-    /* 5. Gallery Active */
-    var client_list = $('.completed-active');
-    if (client_list.length) {
-        client_list.owlCarousel({
-            slidesToShow: 2,
-            slidesToScroll: 1,
-            loop: true,
-            autoplay: true,
-            speed: 3000,
-            smartSpeed: 2000,
-            nav: false,
-            dots: false,
-            margin: 15,
-
-            autoplayHoverPause: true,
-            responsive: {
-                0: {
-                    items: 1
-                },
-                768: {
-                    items: 2
-                },
-                992: {
-                    items: 2
-                },
-                1200: {
-                    items: 3
-                }
-            }
-        });
-    }
-
-
-    /* 6. Nice Selectorp  */
-    var nice_Select = $('select');
-    if (nice_Select.length) {
-        nice_Select.niceSelect();
-    }
-
-    /* 7.  Custom Sticky Menu  */
-    $(window).on('scroll', function () {
-        var scroll = $(window).scrollTop();
-        if (scroll < 245) {
-            $(".header-sticky").removeClass("sticky-bar");
-        } else {
-            $(".header-sticky").addClass("sticky-bar");
-        }
-    });
-
-    $(window).on('scroll', function () {
-        var scroll = $(window).scrollTop();
-        if (scroll < 245) {
-            $(".header-sticky").removeClass("sticky");
-        } else {
-            $(".header-sticky").addClass("sticky");
-        }
-    });
-
-
-    /* 8. sildeBar scroll */
+/* 8. sildeBar scroll */
+function scrollToUp() {
     $.scrollUp({
         scrollName: 'scrollUp', // Element ID
         topDistance: '300', // Distance from top before showing element (px)
@@ -205,37 +78,6 @@
         scrollText: '<i class="ti-arrow-up"></i>', // Text for element
         activeOverlay: false, // Set CSS color to display scrollUp active point, e.g '#00FFFF'
     });
+}
 
 
-    /* 9. data-background */
-    $("[data-background]").each(function () {
-        $(this).css("background-image", "url(" + $(this).attr("data-background") + ")")
-    });
-
-
-    /* 10. WOW active */
-    new WOW().init();
-
-    /* 11. Datepicker */
-
-// 11. ---- Mailchimp js --------//
-    function mailChimp() {
-        $('#mc_embed_signup').find('form').ajaxChimp();
-    }
-
-    mailChimp();
-
-
-// 12 Pop Up Img
-    var popUp = $('.single_gallery_part, .img-pop-up');
-    if (popUp.length) {
-        popUp.magnificPopup({
-            type: 'image',
-            gallery: {
-                enabled: true
-            }
-        });
-    }
-
-
-})(jQuery);
