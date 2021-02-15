2e4317a73418403fa5e2ecd0078f3a761aab42f4
anton <23635473+1progon@users.noreply.github.com>
2021-02-15 19:33:25 +0500

    Add admin layout view

---
+++ resources/views/layouts/admin-layout.blade.php
@@ -0,0 +1,31 @@
+<!doctype html>
+<html lang="en">
+
+@include('include.head')
+
+<body>
+
+@include('include.header')
+
+
+<main class="container-fluid d-flex flex-row flex-wrap my-3">
+    <div class="col-12 col-md-3 my-2">
+        @include('include.admin.admin-sidebar')
+    </div>
+    <div class="col my-2">
+        @yield('main')
+    </div>
+</main>
+
+@include('include.footer')
+
+@yield('scripts_before')
+@include('include.scripts')
+@yield('scripts_after')
+
+
+<script defer src="{{ asset('assets/js/main.js') }}"></script>
+
+
+</body>
+</html>
