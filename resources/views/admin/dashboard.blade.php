5a011976788872cff72706f36ad7ae54709b7e9c
anton <23635473+1progon@users.noreply.github.com>
2021-02-15 18:12:27 +0500

    Move dashboard file into admin views

---
+++ resources/views/admin/dashboard.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.admin-layout')
+
+@section('main')
+    <div class="card">
+        <div class="card-header">Аккаунт</div>
+
+        <div class="card-body">
+            @if ( session('status'))
+                <div class="alert alert-success" role="alert">
+                    {{ session('status') }}
+                </div>
+            @endif
+            Вы вошли в систему!
+        </div>
+    </div>
+@endsection
