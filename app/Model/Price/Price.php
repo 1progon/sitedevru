274aa3e71f1b7726d3c1b7b6f630c2d8282fd59d
anton <23635473+1progon@users.noreply.github.com>
2021-02-16 15:27:17 +0500

    Add fillable into Price model

--- app/Model/Price/Price.php
+++ app/Model/Price/Price.php
@@ -6,6 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Price extends Model
 {
+
+    protected $fillable = [
+        'slug',
+        'title',
+        'description',
+        'on_homepage',
+        'price',
+        'img',
+    ];
     public function getRouteKeyName()
     {
         return 'slug';
