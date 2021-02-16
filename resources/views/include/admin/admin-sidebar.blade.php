c1432561e14baddc2491bedde4fc474c905c73b3
anton <23635473+1progon@users.noreply.github.com>
2021-02-16 21:22:49 +0500

    Admin Sidebar: add active CSS class depends on route name

--- resources/views/include/admin/admin-sidebar.blade.php
+++ resources/views/include/admin/admin-sidebar.blade.php
@@ -8,44 +8,55 @@
         </div>
         <div class="list-group">
 
+            @php
+                $route = Route::currentRouteName();
+            @endphp
+
             <div class="list-group list-group-flush">
-                <div class="list-group-item list-group-item-info font-weight-bold">Страница</div>
+                <div class="list-group-item list-group-item-secondary font-weight-bold">Страница</div>
                 <a href="{{ route('pages.create') }}"
-                   class="list-group-item list-group-item-action"
+                   class="list-group-item list-group-item-action @if( $route == 'pages.create') active @endif"
                    aria-current="true">Добавить страницу</a>
 
                 <a href="{{ route('pages.admin.index') }}"
-                   class="list-group-item list-group-item-action">Все страницы</a>
+                   class="list-group-item list-group-item-action @if( $route == 'pages.admin.index') active @endif">Все
+                    страницы</a>
             </div>
 
             <div class="list-group list-group-flush">
-                <div class="list-group-item list-group-item-info font-weight-bold">Шаблон</div>
+                <div class="list-group-item list-group-item-secondary font-weight-bold">Шаблон</div>
                 <a href="{{ route('templates.create') }}"
-                   class="list-group-item list-group-item-action">Добавить шаблон</a>
+                   class="list-group-item list-group-item-action @if( $route == 'templates.create') active @endif">Добавить
+                    шаблон</a>
 
                 <a href="{{ route('templates.admin.index') }}"
-                   class="list-group-item list-group-item-action">Все шаблоны</a>
+                   class="list-group-item list-group-item-action @if( $route == 'templates.admin.index') active @endif">Все
+                    шаблоны</a>
             </div>
 
 
             <div class="list-group list-group-flush">
-                <div class="list-group-item list-group-item-info font-weight-bold">Услуга</div>
+                <div class="list-group-item list-group-item-secondary font-weight-bold">Услуга</div>
                 <a href="{{ route('services.create') }}"
-                   class="list-group-item list-group-item-action">Добавить услугу</a>
+                   class="list-group-item list-group-item-action @if( $route == 'services.create') active @endif">Добавить
+                    услугу</a>
 
 
                 <a href="{{ route('services.admin.index') }}"
-                   class="list-group-item list-group-item-action">Все услуги</a>
+                   class="list-group-item list-group-item-action @if( $route == 'services.admin.index') active @endif">Все
+                    услуги</a>
             </div>
 
 
             <div class="list-group list-group-flush">
-                <div class="list-group-item list-group-item-info font-weight-bold">Цена</div>
+                <div class="list-group-item list-group-item-secondary font-weight-bold">Цена</div>
                 <a href="{{ route('prices.create') }}"
-                   class="list-group-item list-group-item-action">Добавить цену</a>
+                   class="list-group-item list-group-item-action @if( $route == 'prices.create') active @endif">Добавить
+                    цену</a>
 
                 <a href="{{ route('prices.admin.index') }}"
-                   class="list-group-item list-group-item-action">Все цены</a>
+                   class="list-group-item list-group-item-action @if( $route == 'prices.admin.index') active @endif">Все
+                    цены</a>
             </div>
 
 
