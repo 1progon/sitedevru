3cfac8f0f07e77affef8668d410879dcaa440654
anton <23635473+1progon@users.noreply.github.com>
2021-02-16 14:20:11 +0500

    Add fillable array in Template model

--- app/Model/Template/Template.php
+++ app/Model/Template/Template.php
@@ -8,6 +8,16 @@ class Template extends Model
 {
     public $table = 'templates';
 
+    protected $fillable = [
+        'name',
+        'description',
+        'article',
+        'slug',
+        'on_homepage',
+        'img',
+        'img2',
+    ];
+
     public function getRouteKeyName()
     {
         return 'slug';
