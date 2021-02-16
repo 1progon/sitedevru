8c6f832cf59e99ad0c8bf191db91ee7eb0a8b4dd
anton <23635473+1progon@users.noreply.github.com>
2021-02-16 15:27:31 +0500

    Add fillable into Service model

--- app/Model/Service/Service.php
+++ app/Model/Service/Service.php
@@ -6,6 +6,15 @@ use Illuminate\Database\Eloquent\Model;
 
 class Service extends Model
 {
+
+    protected $fillable = [
+        'title',
+        'slug',
+        'on_homepage',
+        'description',
+        'img',
+        'img2',
+    ];
     public function getRouteKeyName()
     {
         return 'slug';
