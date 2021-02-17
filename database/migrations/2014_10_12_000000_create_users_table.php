5f77ed21e4953dbd441df94ce7ca198a83d66953
anton <23635473+1progon@users.noreply.github.com>
2021-02-17 17:01:05 +0500

    Update Users table migration
    Change name to login column
    Set active default to active (1)
    Set default role by Users roles array

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Model\User\User;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
@@ -17,7 +18,7 @@ class CreateUsersTable extends Migration
             'users',
             function (Blueprint $table) {
                 $table->bigIncrements('id');
-                $table->string('name');
+                $table->string('login')->unique();
 
                 $table->string('first_name')->nullable();
                 $table->string('last_name')->nullable();
@@ -27,9 +28,9 @@ class CreateUsersTable extends Migration
                 $table->timestamp('email_verified_at')->nullable();
                 $table->string('password');
 
-                $table->boolean('active')->default(0);
+                $table->boolean('active')->default(1);
 
-                $table->unsignedTinyInteger('role')->default(0);
+                $table->unsignedTinyInteger('role')->default(User::ROLE['user']);
 
 
                 $table->rememberToken();
