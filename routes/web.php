99b9814771ec33baa5eedbf9666fd5a183bf324d
anton <23635473+1progon@users.noreply.github.com>
2021-02-17 16:52:42 +0500

    Add routes for account user panel

--- routes/web.php
+++ routes/web.php
@@ -11,6 +11,9 @@
 |
 */
 
+use App\Http\Controllers\Account\DashboardAccountController;
+use App\Http\Controllers\Errors\ErrorsController;
+use App\Http\Controllers\Other\SendMessage;
 use App\Model\User\User;
 use App\Http\Controllers\Admin\DashboardController;
 use App\Http\Controllers\HomepageController;
@@ -19,9 +22,15 @@ use Illuminate\Support\Facades\Route;
 
 Auth::routes();
 
-Route::get('/', [HomepageController::class, 'index'])->name('homepage');
-Route::post('/send-message', [\App\Http\Controllers\Other\SendMessage::class, 'postMessage'])
+Route::get('/', [HomepageController::class, 'index'])
+    ->name('homepage');
+Route::post('/send-message', [SendMessage::class, 'postMessage'])
     ->name('message.send');
+
+Route::get('no-access', [ErrorsController::class, 'noAccess'])
+    ->name('no-access');
+Route::get('not-active-user', [ErrorsController::class, 'notActiveUser'])
+    ->name('not-active-user');
 
 
 //Main pages in views
@@ -29,8 +38,10 @@ Route::name('pages.')->group(
     function () {
         Route::prefix('pages')->group(
             function () {
-                Route::get('contact', 'Pages\PagesController@contact')->name('contact');
-                Route::get('pricing', 'Pages\PagesController@pricing')->name('pricing');
+                Route::get('contact', 'Pages\PagesController@contact')
+                    ->name('contact');
+                Route::get('pricing', 'Pages\PagesController@pricing')
+                    ->name('pricing');
             }
         );
     }
@@ -38,23 +49,44 @@ Route::name('pages.')->group(
 
 
 //User account panel
-Route::middleware(['auth', 'role:' . User::ROLE_USER])
+Route::middleware(['auth', 'active_or_not', 'access_roles:' . User::ROLE['user'] . ',' . User::ROLE['admin']])
     ->prefix('account')
     ->group(
         function () {
-            Route::get('dashboard', [DashboardController::class, 'index'])
+            Route::get('dashboard', [DashboardAccountController::class, 'index'])
                 ->name('dashboard.user');
+
+            Route::get('edit-data', [DashboardAccountController::class, 'editData'])
+                ->name('account.edit.data');
+
+            Route::put('update-data', [DashboardAccountController::class, 'updateData'])
+                ->name('account.update.data');
+
+            Route::get('edit-password', [DashboardAccountController::class, 'editPassword'])
+                ->name('account.edit.password');
+
+            Route::put('update-password', [DashboardAccountController::class, 'updatePassword'])
+                ->name('account.update.password');
+
+            Route::get('edit-email', [DashboardAccountController::class, 'editEmail'])
+                ->name('account.edit.email');
+
+            Route::put('update-email', [DashboardAccountController::class, 'updateEmail'])
+                ->name('account.update.email');
         }
     );
 
 
 //Admin account panel
-Route::middleware(['auth', 'role:' . User::ROLE_ADMIN])
+Route::middleware(['auth', 'active_or_not', 'access_roles:' . User::ROLE['admin']])
     ->prefix('admin')
     ->group(
         function () {
             Route::get('dashboard', [DashboardController::class, 'index'])
                 ->name('dashboard.admin');
+
+            Route::get('users', 'Users\UsersController@adminIndex')
+                ->name('admin.users');
 
             Route::resource('pages', 'Pages\PagesController')
                 ->except(['index', 'show', 'pricing', 'contact']);
