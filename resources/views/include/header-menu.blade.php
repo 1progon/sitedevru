efae747ef251420f5b7b8c51fabecd44e74691ac
anton <23635473+1progon@users.noreply.github.com>
2021-02-18 11:35:41 +0500

    Add popup class to header menu links

--- resources/views/include/header-menu.blade.php
+++ resources/views/include/header-menu.blade.php
@@ -2,12 +2,12 @@
 <div class="main-menu d-none d-lg-block">
     <nav>
         <ul id="navigation">
-            <li><a href="{{ route('pages.show', 'about') }}">О нас</a></li>
-            <li><a href="{{ route('services.index') }}">Услуги</a></li>
-            <li><a href="{{ route('prices.index') }}">Цены</a></li>
-            <li><a href="{{ route('templates.index') }}">Шаблоны</a></li>
+            <li><a class="popup" href="{{ route('pages.show', 'about') }}">О нас</a></li>
+            <li><a class="popup" href="{{ route('services.index') }}">Услуги</a></li>
+            <li><a class="popup" href="{{ route('prices.index') }}">Цены</a></li>
+            <li><a class="popup" href="{{ route('templates.index') }}">Шаблоны</a></li>
 
-            <li><a href="{{ route('blog') }}">Блог</a>
+            <li><a class="popup" href="{{ route('blog') }}">Блог</a>
                 {{--                <ul class="submenu">--}}
                 {{--                    <li><a href="">Blog</a></li>--}}
                 {{--                    <li><a href="">Blog Details</a></li>--}}
@@ -28,19 +28,19 @@
                 </form>
 
                 @if( auth()->user()->role === \App\Model\User\User::ROLE['user'] )
-                    <li><a href="{{ route('dashboard.user') }}">Аккаунт</a></li>
+                    <li><a class="popup" href="{{ route('dashboard.user') }}">Аккаунт</a></li>
                 @elseif( auth()->user()->role === \App\Model\User\User::ROLE['admin'] )
-                    <li><a href="{{ route('dashboard.user') }}">Аккаунт</a></li>
-                    <li><a href="{{ route('dashboard.admin') }}">Админ</a></li>
+                    <li><a class="popup" href="{{ route('dashboard.user') }}">Аккаунт</a></li>
+                    <li><a class="popup" href="{{ route('dashboard.admin') }}">Админ</a></li>
                 @endif
 
-                <button class="btn-danger" form="logout-form" type="submit">Выйти</button>
+                <button class="btn btn-danger" form="logout-form" type="submit">Выйти</button>
 
 
 
             @else
-                <li><a href="{{ route('login') }}">Вход</a></li>
-                <li><a href="{{ route('register') }}">Регистрация</a></li>
+                <li><a class="popup" href="{{ route('login') }}">Вход</a></li>
+                <li><a class="popup" href="{{ route('register') }}">Регистрация</a></li>
             @endauth
         </ul>
     </nav>
