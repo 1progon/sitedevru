4a1d49a3c0dad2d9f2f723526844d09013b9de8a
Anton <23635473+1progon@users.noreply.github.com>
2021-02-24 20:36:53 +0500

    Update public js

--- public/js/app.js
+++ public/js/app.js
@@ -49886,8 +49886,8 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/anton/DATA D 1TB/С Диска Е NVME/CodingProjects/PhpStormProjects/getsitedev ru/resources/js/app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! /home/anton/DATA D 1TB/С Диска Е NVME/CodingProjects/PhpStormProjects/getsitedev ru/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/anton/NVME ext4/CodingProjects/PhpStormProjects/getsitedev ru/resources/js/app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! /home/anton/NVME ext4/CodingProjects/PhpStormProjects/getsitedev ru/resources/sass/app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
