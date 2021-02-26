8753c6a23acb47e07baed9e80846df047b2590f4
Anton <23635473+1progon@users.noreply.github.com>
2021-02-26 16:29:47 +0500

    Update account sidebar css class and update menu on new links

--- resources/views/include/account/account-sidebar.blade.php
+++ resources/views/include/account/account-sidebar.blade.php
@@ -1,4 +1,4 @@
-<div class="">
+<div class="account-sidebar">
 
     <div class="card">
         <div class="card-header">
@@ -25,7 +25,59 @@
                    class="list-group-item list-group-item-action @if( $route == 'account.edit.email') active @endif"
                    aria-current="true">Изменить почту</a>
 
+                <a href="#"
+                   class="list-group-item list-group-item-action @if( $route == '') active @endif"
+                   aria-current="true">Уведомления</a>
+            </div>
 
+
+            <div class="list-group list-group-flush">
+                <div class="list-group-item list-group-item-secondary font-weight-bold">Заказы</div>
+
+                <a href="#"
+                   class="list-group-item list-group-item-action @if( $route == '') active @endif"
+                   aria-current="true">Сделать заказ</a>
+
+                <a href="#"
+                   class="list-group-item list-group-item-action @if( $route == '') active @endif"
+                   aria-current="true">Активные заказы</a>
+
+                <a href="#"
+                   class="list-group-item list-group-item-action @if( $route == '') active @endif"
+                   aria-current="true">Ожидают оплаты</a>
+
+                <a href="#"
+                   class="list-group-item list-group-item-action @if( $route == '') active @endif"
+                   aria-current="true">Архив заказов</a>
+
+                <a href="#"
+                   class="list-group-item list-group-item-action @if( $route == '') active @endif"
+                   aria-current="true">Все заказы</a>
+
+            </div>
+
+            <div class="list-group list-group-flush">
+                <div class="list-group-item list-group-item-secondary font-weight-bold">Поддержка</div>
+
+                <a href="#"
+                   class="list-group-item list-group-item-action @if( $route == '') active @endif"
+                   aria-current="true">Создать обращение</a>
+
+                <a href="#"
+                   class="list-group-item list-group-item-action @if( $route == '') active @endif"
+                   aria-current="true">Все обращения</a>
+            </div>
+
+            <div class="list-group list-group-flush">
+                <div class="list-group-item list-group-item-secondary font-weight-bold">Аккаунт</div>
+
+                <a href="#"
+                   class="list-group-item list-group-item-action @if( $route == '') active @endif"
+                   aria-current="true">Временное отключение</a>
+
+                <a href="#"
+                   class="list-group-item list-group-item-action @if( $route == '') active @endif"
+                   aria-current="true">Удаление</a>
             </div>
 
 
