d872fa06bbde45bc4eb7d1ab1969762491a0ca00
Anton <23635473+1progon@users.noreply.github.com>
2021-02-26 15:42:31 +0500

    Remove img from portfolio seeder and add on_homepage default

--- database/seeds/PortfolioSeeder.php
+++ database/seeds/PortfolioSeeder.php
@@ -17,44 +17,44 @@ class PortfolioSeeder extends Seeder
             [
                 'title' => 'Наша работа 1',
                 'slug' => Str::slug('Наша работа 1'),
-                'img' => 'assets/img/visit/visit_1.jpg',
                 'url' => 'javascript:void(0)',
                 'description' => '',
+                'on_homepage' => 1
             ],
             [
                 'title' => 'Наша работа 2',
                 'slug' => Str::slug('Наша работа 2'),
-                'img' => 'assets/img/visit/visit_1.jpg',
                 'url' => 'javascript:void(0)',
                 'description' => '',
+                'on_homepage' => 1
             ],
             [
                 'title' => 'Наша работа 3',
                 'slug' => Str::slug('Наша работа 3'),
-                'img' => 'assets/img/visit/visit_1.jpg',
                 'url' => 'javascript:void(0)',
                 'description' => '',
+                'on_homepage' => 1
             ],
             [
                 'title' => 'Наша работа 4',
                 'slug' => Str::slug('Наша работа 4'),
-                'img' => 'assets/img/visit/visit_1.jpg',
                 'url' => 'javascript:void(0)',
                 'description' => '',
+                'on_homepage' => 1
             ],
             [
                 'title' => 'Наша работа 5',
                 'slug' => Str::slug('Наша работа 5'),
-                'img' => 'assets/img/visit/visit_1.jpg',
                 'url' => 'javascript:void(0)',
                 'description' => '',
+                'on_homepage' => 1
             ],
             [
                 'title' => 'Наша работа 6',
                 'slug' => Str::slug('Наша работа 6'),
-                'img' => 'assets/img/visit/visit_1.jpg',
                 'url' => 'javascript:void(0)',
                 'description' => '',
+                'on_homepage' => 1
             ],
 
         ];
