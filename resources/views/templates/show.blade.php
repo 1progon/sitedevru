20538157f1cdfc9b877eeb1c0c185787e42bfae4
Anton <23635473+1progon@users.noreply.github.com>
2021-03-01 20:20:05 +0500

    Update view template page wit html css layout

--- resources/views/templates/show.blade.php
+++ resources/views/templates/show.blade.php
@@ -11,38 +11,85 @@
             <div class="col-lg-7">
                 <div class="section-tittle text-center">
                     <h2>{{ $template->name }}</h2>
-                    <h3 class="description">ID: {{ $template->id }}</h3>
+                    <h4 class="description">ID: {{ $template->id }}</h4>
                 </div>
             </div>
         </div>
 
-        {{--TODO Hardcoded Max width thumbs--}}
-        <div>
-
-            <h2>Иконки сайта. Клик, чтобы развернуть</h2>
-
-            @if( $template->img )
-                <a data-fancybox="gallery" href="{{ asset('storage/' . $template->img) }}">
-                    <img style="max-width: 200px" src="{{ asset('storage/' . $template->img) }}" alt="первое изображение
-                шаблона">
-                </a>
-            @endif
-
-            @if( $template->img2 )
-                <a data-fancybox="gallery" href="{{ asset('storage/' . $template->img2) }}">
-                    <img style="max-width: 200px" src="{{ asset('storage/' . $template->img2) }}" alt="второе изображение
-                шаблона">
-                </a>
-            @endif
-
-
-        </div>
 
         <div>
             <div class="description">{!!  $template->description !!}</div>
+        </div>
 
+        {{--Page panels--}}
+        <div class="row">
+
+            {{--Left big images and thumbs--}}
+            <div class="col-lg-6">
+                <div class="about-feature mt-30">
+                    <div class="about-feature-image">
+                        @if( $template->img)
+                            <a data-fancybox="gallery"
+                               href="{{ asset( 'storage/' . $template->img) }}">
+                                <img src="{{ asset( 'storage/' . $template->img) }}"
+                                     width="100%" alt="">
+
+                            </a>
+                        @endif
+                    </div>
+                    <div class="about-feature-thumbs my-3">
+                        @if( $template->img2)
+                            <a data-fancybox="gallery"
+                               href="{{ asset( 'storage/' . $template->img2) }}">
+                                <img src="{{ asset( 'storage/' . $template->img2) }}"
+                                     width="100" alt="">
+                            </a>
+                        @else
+                            <img src="{{ asset('assets/img/icons/image-placeholder.svg')}}"
+                                 width="100" alt="">
+                            <img src="{{ asset('assets/img/icons/image-placeholder.svg')}}"
+                                 width="100" alt="">
+                            <img src="{{ asset('assets/img/icons/image-placeholder.svg')}}"
+                                 width="100" alt="">
+                        @endif
+                    </div>
+                    <div class="about-feature-content">
+                        <h2 class="feature-title">The future of designing starts here</h2>
+                        <p class="text">An open platform for presentations and content collaboration. Sign up to get
+                            early access.</p>
+                    </div>
+                </div>
+            </div>
+
+            {{--Right content--}}
+            <div class="col-lg-6">
+
+
+                {{--TODO Hardcoded dummy data--}}
+                @forelse( $templateParts as $part)
+                    <div class="about-feature-items d-sm-flex mt-30">
+                        <div class="feature-items-icon">
+                            <img class="img80"
+                                 src="{{ $part->img }}"
+                                 alt="Icon">
+                        </div>
+                        <div class="feature-items-content media-body">
+                            <h5 class="items-title">{{ $part->title }}</h5>
+                            <p class="text">{{ $part->text }}</p>
+                        </div>
+                    </div>
+                @empty
+                @endforelse
+
+
+            </div>
+        </div>
+
+
+        <div class="row">
             <article>{!! $template->article !!}</article>
         </div>
+
 
     </div>
 @endsection
