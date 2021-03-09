bb759f9d0412bbf1f89fdfeb782e24ebc85809a8
Anton <23635473+1progon@users.noreply.github.com>
2021-03-09 20:28:07 +0500

    Update CSS: Add bold to a href links in article tag

--- public/js/app.js
+++ public/js/app.js
@@ -49908,10 +49908,10 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! /home/anton/NVME ext4/CodingProjects/PhpStormProjects/getsitedev ru/resources/js/app.js */"./resources/js/app.js");
-__webpack_require__(/*! /home/anton/NVME ext4/CodingProjects/PhpStormProjects/getsitedev ru/resources/sass/app.scss */"./resources/sass/app.scss");
-__webpack_require__(/*! /home/anton/NVME ext4/CodingProjects/PhpStormProjects/getsitedev ru/resources/sass/admin/admin-app.scss */"./resources/sass/admin/admin-app.scss");
-module.exports = __webpack_require__(/*! /home/anton/NVME ext4/CodingProjects/PhpStormProjects/getsitedev ru/resources/sass/account/account-app.scss */"./resources/sass/account/account-app.scss");
+__webpack_require__(/*! /home/anton/NVME ntfs/CodingProjects/PhpStormProjects/getsitedev ru/resources/js/app.js */"./resources/js/app.js");
+__webpack_require__(/*! /home/anton/NVME ntfs/CodingProjects/PhpStormProjects/getsitedev ru/resources/sass/app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! /home/anton/NVME ntfs/CodingProjects/PhpStormProjects/getsitedev ru/resources/sass/admin/admin-app.scss */"./resources/sass/admin/admin-app.scss");
+module.exports = __webpack_require__(/*! /home/anton/NVME ntfs/CodingProjects/PhpStormProjects/getsitedev ru/resources/sass/account/account-app.scss */"./resources/sass/account/account-app.scss");
 
 
 /***/ })
